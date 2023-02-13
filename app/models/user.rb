2124f8c40805c42466404c16c0be455c2595aec6
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-13 14:30:42 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true,length: { maximum: 75 } 
+  validates :addrress, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
