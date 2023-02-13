39133b4bc5fbbf4227a1e60a481afd895f9362ea
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-13 17:49:33 +0900

    new画面作成#3

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,2 +1,24 @@
 class ThemesController < ApplicationController
+    def index
+      @theme = Theme.all
+    end
+
+    def new
+      @theme = Theme.new
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+
+    def edit
+    end
+
+    def destroy
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root to: "themes#index"
   resources :themes
+  
 
   devise_for :users
   if Rails.env.development?
