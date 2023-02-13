e4dce93ae7838189cb3e837c91a74b6d6a2a6a9d
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-13 14:26:12 +0900

    ストロングパラメーター

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
 end
