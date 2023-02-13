a8529bf1481daa579d25a75784e197f2e15fe21a
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-13 14:06:46 +0900

    device導入,viewファイル生成#1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
