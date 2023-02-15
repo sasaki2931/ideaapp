99654db1e0a8eb07ca41d9e7ad844b9c9f9a3dda
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-15 15:59:49 +0900

    モデル追記#9

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,6 @@
 class Theme < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :labellings, :dependent: :destroy
+  has_many :labels, through: :labellings
 end
