841a1a59c4edc57a8920877aaa2983e1a1c2a5f1
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-20 07:45:44 +0900

    アクセス制限#27

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,32 +1,8 @@
 RailsAdmin.config do |config|
   config.asset_source = :webpacker
-
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == CancanCan ==
-  # config.authorize_with :cancancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/railsadminteam/rails_admin/wiki/Base-configuration
-
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
-
   config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
+    dashboard                     
+    index                         
     new
     export
     bulk_delete
@@ -34,9 +10,5 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
   end
 end
