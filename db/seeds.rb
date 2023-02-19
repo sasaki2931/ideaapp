50737040dfc45237512f8c52116fb1fe144982fe
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-20 03:38:24 +0900

    seedデータ投入#19

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,19 @@ Label.create!([
     {name:'勉強'},
     {name:'趣味'}
 ])
+
+20.times do |n|
+    user = User.create!(
+      name: "User#{n+1}",
+      email: "user#{n+1}@example.com",
+      password: "password",
+      address: "Address#{n+1}"
+    )
+    Theme.create!(
+      title: "test#{n+1}",
+      content: "test#{n+1}",
+      reword: "test#{n+1}",
+      img: "test#{n+1}",
+      user_id: user.id
+    )
+  end
