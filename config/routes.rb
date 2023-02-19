aed3376a13aadab158cafc6008261c332dd60189
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-20 04:24:36 +0900

    root,view作成#21

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to: "tops#index"
   resource :tops, only: [:show]
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy,:index]
   resources :themes do
     resources :ideas, only: [:create, :destroy]
   end
   
   
   devise_for :users
-  resources :users, only: [:index,:show]
+  resources :users, only: [:show]
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
