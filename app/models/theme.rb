18d8ad9194d1d0d37d33874de1645bd89f9f932f
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-24 04:01:49 +0900

    themeバリデーション追加#51

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,6 @@
 class Theme < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :labellings, dependent: :destroy
