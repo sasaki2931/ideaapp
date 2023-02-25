49dad513c0b6b0b827c1ddc1bc6c7500a62e0cba
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-25 14:48:08 +0900

    theme入力欄修正#63

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
     post 'send_email', on: :member
     resources :ideas
   end
-  
-  
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'sessions#new_guest'
