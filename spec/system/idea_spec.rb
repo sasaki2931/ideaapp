0c34b4f5aac0ab530d8ee0afb933446aa631ea49
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-27 04:37:15 +0900

    ideaspec実装

--- spec/system/idea_spec.rb
+++ spec/system/idea_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'アイデア送信管理機能', type: :system do
     let(:user) {FactoryBot.create(:user)}
-    let(:theme) {FactoryBot.create(:theme)}
+    let(:user2) {FactoryBot.create(:user2)}
+    let(:theme) {FactoryBot.create(:theme,user: user2)}
     before do
         visit root_path
         sign_in user
@@ -9,63 +10,30 @@ RSpec.describe 'アイデア送信管理機能', type: :system do
     describe 'アイデア投稿機能' do
         context 'アイデアを投稿した場合' do
           it '作成したアイデアが表示される' do
-          visit new_theme_path
-          fill_in'theme[title]',with:'test'
-          fill_in'theme[content]',with:'test'
-          fill_in'theme[reword]',with:'test'
-          check 'theme_label_ids_1'
-          click_button'登録する'
-          expect(page).to have_content 'test'
+            visit theme_path(theme)
+            click_on '参加する'
+            fill_in'idea[title]',with:'test'
+            fill_in'idea[content]',with:'test'
+            click_button'登録する'
+            expect(page).to have_content 'test'
+          end
         end
-      end
-    end
-    describe '一覧表示機能' do
-      context '一覧画面に遷移した場合' do
-        it '作成済みのタスク一覧が表示される' do
-          theme = FactoryBot.create(:theme, user: user)
-          visit themes_path
-          expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
-        end
-      end
-      context '募集が作成日時の降順に並んでいる場合' do
-        it '新しいタスクが一番上に表示される' do
-          new_theme = FactoryBot.create(:theme, user: user, title: '新しい募集')
-          visit themes_path
-          theme_list = all('.theme_row') 
-          expect(theme_list[0]).to have_content '新しい募集'
-        end
-      end
     end
     describe '詳細表示機能' do
-      context '任意の募集詳細画面に遷移した場合' do
-        it '該当タスクの内容が表示される' do
+      context '一覧画面に遷移した場合' do
+        it '作成済みのアイデア一覧が表示される' do
           theme = FactoryBot.create(:theme, user: user)
-          visit theme_path(theme.id)
-
+          idea = FactoryBot.create(:idea,theme: theme, user: user)
+          visit theme_ideas_path(theme)
           expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
         end
-      end
-    end
-    describe 'メール送信機能' do
-      context '自分が投稿した募集詳細画面に遷移した場合' do
-        it '該当募集の内容が表示される' do
-          new_theme = FactoryBot.create(:theme, user: user, title: '新しい募集')
-          theme = FactoryBot.create(:theme, user: user)
-          idea = FactoryBot.create(:idea, theme: theme, user: user)
-          visit my_theme_path(theme.id)
-          expect(page).to have_content theme.title
+        it '募集の詳細を見れる' do
+          theme = FactoryBot.create(:theme, user: user2)
+          idea = FactoryBot.create(:idea,theme: theme, user: user)
+          visit theme_ideas_path(theme)
+          click_link "募集詳細"
+          expect(page).to have_content 'デフォルト1'
         end
-      end
-      context 'アイデアを選択した場合メールが送信される' do
-        it '該当募集のアイデアが表示される' do
-          new_theme = FactoryBot.create(:theme, user: user, title: '新しい募集')
-          theme = FactoryBot.create(:theme, user: user)
-          idea = FactoryBot.create(:idea, theme: theme, user: user)
-          visit my_theme_path(theme.id)
-          check "idea_ids_#{idea.id}"
-          click_on '採用'
-          expect(page).to have_content 'メールを送信しました'
-        end
-      end
+      end  
     end
 end
