0435875248cf7c46981922cfd045a1b9c4fe74b6
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-27 09:13:59 +0900

    themerspec

--- spec/system/theme_spec.rb
+++ spec/system/theme_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 RSpec.describe '募集管理機能', type: :system do
     let(:user) {FactoryBot.create(:user)}
-    
+    let(:label) {FactoryBot.create(:label)}
     before do
-        visit root_path
+       root_path
         sign_in user
     end
     describe '新規作成機能' do
         context '募集を新規作成した場合' do
-            it '作成した募集が表示される' do
+          it '作成した募集が表示される' do
+          label = 
           visit new_theme_path
           fill_in'theme[title]',with:'test'
           fill_in'theme[content]',with:'test'
           fill_in'theme[reword]',with:'test'
-          check 'theme_label_ids_1'
           click_button'登録する'
           expect(page).to have_content 'test'
         end
@@ -25,15 +25,6 @@ RSpec.describe '募集管理機能', type: :system do
           theme = FactoryBot.create(:theme, user: user)
           visit themes_path
           expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
-        end
-      end
-    end
-      context '募集が作成日時の降順に並んでいる場合' do
-        it '新しいタスクが一番上に表示される' do
-          new_theme = FactoryBot.create(:theme, user: user, title: '新しい募集')
-          visit themes_path
-          theme_list = all('.theme_row') 
-          expect(theme_list[0]).to have_content '新しい募集'
         end
       end
     end
@@ -58,12 +49,13 @@ RSpec.describe '募集管理機能', type: :system do
       end
       context 'アイデアを選択した場合メールが送信される' do
         it '該当募集のアイデアが表示される' do
-          new_theme = FactoryBot.create(:theme, user: user, title: '新しい募集')
           theme = FactoryBot.create(:theme, user: user)
-          idea = FactoryBot.create(:idea, theme: theme, user: user)
+          user2 = FactoryBot.create(:user2)
+          idea = FactoryBot.create(:idea, theme: theme, user: user2)
           visit my_theme_path(theme.id)
-          check "idea_ids_#{idea.id}"
-          click_on '採用'
+          click_link '詳細を確認する'
+          check "idea_ids[]"
+          click_on '選択したアイデアにメールを送る'
           expect(page).to have_content 'メールを送信しました'
         end
       end
